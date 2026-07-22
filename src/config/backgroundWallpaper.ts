a3c465b90e2aaf7a60d46c584690b09ed2cb8371
wuchan123 <461001498@qq.com>
2026-07-23 00:01:51 +0800

    tp

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -43,12 +43,12 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/d1.jpg"
+			"./d1.jpg"
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/m1.jpg"
+			"./m1.jpg"
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组）
